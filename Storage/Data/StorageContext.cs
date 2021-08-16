f7859d53f8e3f4b34c4184ad3850a67d8e703c38
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-08-16 14:30:34 +0200

    Seed

--- Storage/Data/StorageContext.cs
+++ Storage/Data/StorageContext.cs
@@ -9,11 +9,71 @@ namespace Storage.Data
 {
     public class StorageContext : DbContext
     {
-        public StorageContext (DbContextOptions<StorageContext> options)
+        public StorageContext(DbContextOptions<StorageContext> options)
             : base(options)
         {
         }
 
-        public DbSet<Storage.Models.Entites.Product> Product { get; set; }
+        public DbSet<Product> Product { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().HasData(
+                     new Product
+                     {
+                         Id = 1,
+                         Name = "Iphone 12",
+                         Category = "Mobile",
+                         Count = 156,
+                         Description = "Beskrivning av telefonen här",
+                         OrderDate = DateTime.Now.AddDays(-6),
+                         Price = 15600,
+                         Shelf = "4B"
+                     }, new Product
+                     {
+                         Id = 2,
+                         Name = "S20",
+                         Category = "Mobile",
+                         Count = 98,
+                         Description = "Beskrivning av telefonen här",
+                         OrderDate = DateTime.Now.AddDays(-23),
+                         Price = 12450,
+                         Shelf = "4C"
+                     }, new Product
+                     {
+                         Id = 3,
+                         Name = "MacBook",
+                         Category = "Computers",
+                         Count = 23,
+                         Description = "Beskrivning här",
+                         OrderDate = DateTime.Now.AddDays(-5),
+                         Price = 45600,
+                         Shelf = "4C"
+                     }, new Product
+                     {
+                         Id = 4,
+                         Name = "Suface X",
+                         Category = "Computer",
+                         Count = 32,
+                         Description = "Beskrivning här",
+                         OrderDate = DateTime.Now.AddDays(-40),
+                         Price = 26850,
+                         Shelf = "5C"
+                     }, new Product
+                     {
+                         Id = 5,
+                         Name = "Pen",
+                         Category = "Office Supplies",
+                         Count = 15651,
+                         Description = "Beskrivning här",
+                         OrderDate = DateTime.Now.AddDays(-6),
+                         Price = 1,
+                         Shelf = "45W"
+                     }
+                );
+
+        }
     }
 }
---
+++ Storage/Migrations/20210816122523_SeedData.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Storage.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Product",
+                columns: new[] { "Id", "Category", "Count", "Description", "Name", "OrderDate", "Price", "Shelf" },
+                values: new object[,]
+                {
+                    { 1, "Mobile", 156, "Beskrivning av telefonen här", "Iphone 12", new DateTime(2021, 8, 10, 14, 25, 22, 905, DateTimeKind.Local).AddTicks(5154), 15600, "4B" },
+                    { 2, "Mobile", 98, "Beskrivning av telefonen här", "S20", new DateTime(2021, 7, 24, 14, 25, 22, 908, DateTimeKind.Local).AddTicks(591), 12450, "4C" },
+                    { 3, "Computers", 23, "Beskrivning här", "MacBook", new DateTime(2021, 8, 11, 14, 25, 22, 908, DateTimeKind.Local).AddTicks(635), 45600, "4C" },
+                    { 4, "Computer", 32, "Beskrivning här", "Suface X", new DateTime(2021, 7, 7, 14, 25, 22, 908, DateTimeKind.Local).AddTicks(639), 26850, "5C" },
+                    { 5, "Office Supplies", 15651, "Beskrivning här", "Pen", new DateTime(2021, 8, 10, 14, 25, 22, 908, DateTimeKind.Local).AddTicks(642), 1, "45W" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Product",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
